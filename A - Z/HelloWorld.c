762b41d9fbfafde0647506977586720dd8b9fa57
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-12 23:11:12 +0530

    Print HelloWorld

---
+++ A - Z/HelloWorld.c
@@ -0,0 +1,16 @@
+/*What is C?
+C is a general-purpose programming language created by Dennis Ritchie at the Bell Laboratories in 1972.
+
+It is a very popular language, despite being old.
+
+C is strongly associated with UNIX, as it was developed to write the UNIX operating system.*/
+
+
+#include <stdio.h>
+
+int main(){
+    
+    printf("Hello World!\n");
+    return(0); //exit code of program
+
+}
