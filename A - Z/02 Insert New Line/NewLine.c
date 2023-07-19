c8c2350aeed712b2aad3b71e4d68f828b9c49cac
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-19 05:43:47 +0530

    Create New Line
    To insert a new line, you can use the
    " \n " character.

---
+++ A - Z/02 Insert New Line/NewLine.c
@@ -0,0 +1,27 @@
+//To insert a new line, you can use the \n character:
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("I am learning C.");
+  return 0;
+}
+
+//You can also output multiple lines with a single printf() function. However, this could make the code harder to read:
+/*
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+  return 0;
+}
+
+*/
+/*
+
+\t	--->Creates a horizontal tab
+\\	--->Inserts a backslash character (\)
+\"	--->Inserts a double quote character
+
+*/
