58922f52451fdd893f5a51d948b913a90d09a387
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-19 05:55:40 +0530

    Insert Comment

---
+++ A - Z/03 Comments/Multi_line_Comment.c
@@ -0,0 +1,16 @@
+//Multi-line Comments----->
+
+
+//Multi-line comments start with /* and ends with */.
+
+//Any text between /* and */ will be ignored by the compiler:
+
+
+#include <stdio.h>
+
+int main() {
+  /* The code below will print the words Hello World!
+  to the screen, and it is amazing */
+  printf("Hello World!");
+  return 0;
+}
---
+++ A - Z/03 Comments/Single_line_Comment.c
@@ -0,0 +1,25 @@
+/*
+Comments can be used to explain code, and to make it more readable. It can also be used to prevent execution when testing alternative code.
+
+Comments can be singled-lined or multi-lined.
+*/
+
+//Single-line Comments---->
+
+/*
+Single-line comments start with two forward slashes (//).
+
+Any text between // and the end of the line is ignored by the compiler (will not be executed).
+
+This example uses a single-line comment before a line of code:
+*/
+//Code:--
+
+
+#include <stdio.h>
+
+int main() {
+  // This is a Single-line comment
+  printf("Hello World!");
+  return 0;
+}
