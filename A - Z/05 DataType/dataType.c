a3b1b6ba4d146bcacf90021108336625f34fdb6a
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-22 15:32:51 +0530

    Create Data Types

---
+++ A - Z/05 DataType/dataType.c
@@ -0,0 +1,42 @@
+//Data Types
+
+//Example---->
+#include <stdio.h>
+
+int main() {
+  // Create variables
+  int myNum = 10;               // Integer (whole number)
+  float myFloatNum = 6.99;     // Floating point number
+  char myLetter = 'W';         // Character
+  
+  // Print variables
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  return 0;
+}
+
+
+//Basic Data Types----->
+
+/*The data type specifies the size and type of information the variable will store.*/
+
+//int    -->  2 or 4 bytes	---> Stores whole numbers, without decimals.
+//float	 -->  4 bytes	    ---> Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits.
+//double -->  8 bytes	    ---> Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits.
+//char	 -->  1 byte	    ---> Stores a single character/letter/number, or ASCII values.
+
+//Basic Format Specifiers--->
+
+//There are different format specifiers for each data type. Here are some of them:
+
+/* 
+
+ %d or %i ---> int
+ %f	      ---> float
+ %s       ---> string
+ %lf	  ---> double
+ %c	      ---> char
+
+*/
+
