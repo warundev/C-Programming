ec5b1f02a13603c8e1b6e82716d04b263468b8dc
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-26 17:03:31 +0530

    Entering comments

---
+++ PRACTICAL SESSIONS/02-Comments.c
@@ -0,0 +1,41 @@
+//Practical Number      : 02
+//Problem Specification : Print Hello World using C-programming language.
+
+//Code : 
+
+#include <stdio.h>
+
+int main()
+{
+    printf ("Single-line Commmnts\n"); //This is Single-line Commmnts
+
+    printf ("Multi-line Commmnts");
+    /*
+     This is 
+         Multi-Line
+              Comments
+    */
+    return 0;
+}
+
+
+//Output     : 
+                 /*
+                Single-line Commmnts
+                Multi-line Commmnts
+                */
+
+
+//Conclusion : 
+
+/*
+**Single-line Comments**
+Single-line comments start with two forward slashes (//).
+
+Any text between // and the end of the line is ignored by the compiler (will not be executed).
+*/
+
+// **Multi-line Comments**
+//Multi-line comments start with /* and ends with */.
+
+//Any text between /* and */ will be ignored by the compiler.
