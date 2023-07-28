ee0adae0d48ed43b83c390f111d89707979dd5f3
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-28 13:57:59 +0530

    Using While Loop

---
+++ PRACTICAL SESSIONS/10-WhileLoop.c
@@ -0,0 +1,49 @@
+//Practical Number      : 10
+//Problem Specification : Using While Loop in C language.
+
+//Code  :
+
+#include<stdio.h>
+//While Loop
+/*
+Syntax: while(condition) {
+	statement(s);
+}
+
+variable initialization;
+while(conditition)
+{
+	statemrnts;
+	variable increment or decrement;
+}
+
+*/
+int main()
+{
+	int i =1;
+	
+	while(i<=5){
+		printf("%d\n",i);
+		i++;
+	}
+	return 0;
+}
+	
+	
+
+//Output :
+/*
+
+1
+2
+3
+4
+5
+
+*/
+
+//Conclusion :
+/*
+The above program uses a simple While loop to print the numbers from 1 to 5 separately.
+*/
+
