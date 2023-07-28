bdb2992a955ef9e8cf1cba5858951cf85f86a213
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-28 13:47:47 +0530

    Using ForLoop

---
+++ PRACTICAL SESSIONS/09-ForLoop.c
@@ -0,0 +1,36 @@
+//Practical Number      : 09
+//Problem Specification : Using ForLoop in C language.
+
+
+//Code  :
+
+#include<stdio.h>
+int main()
+{
+	int i;
+	for( i=0;i<=5;i++){	
+	
+	printf("%d\n",i);
+	
+	}
+	return 0;
+}
+
+//Output : 
+/*
+
+0
+1
+2
+3
+4
+5
+
+*/
+
+//Conclusion :
+/*
+
+The for loop is used to execute a set of statements repeatedly until the condition becomes false or it reaches.
+
+*/
