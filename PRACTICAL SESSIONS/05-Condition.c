ce9f9ad4353e3ac2160e83d3eba15216310728c4
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-28 11:48:44 +0530

    Considering condition

---
+++ PRACTICAL SESSIONS/05-Condition.c
@@ -0,0 +1,33 @@
+//Practical Number      : 05
+//Problem Specification : Considering condition using C- language.(if/else)
+
+//Code  :
+
+	#include<stdio.h>
+
+	//if condition
+
+	int main()
+	{
+	    int num1=44;
+	    int num2=30;
+
+	    if(num1<num2){
+		    printf("%i is less than %i ...",num1,num2);
+	    }
+	    else{
+		    printf("%i is less than %i ...",num2,num1);
+	     }
+
+	    return 0;
+
+ 	}
+
+//Output : 30 is less than 44 ...
+
+
+//Conclusion :
+/*
+    In the above code we have used an 'If' statement to check whether a number .
+
+*/
